0e61c3a1420637184a8ed687ffea98042339fc40
Patryk Szynal <patrykszynal97@gmail.com>
2019-11-25 12:40:10 +0100

    Pogoda activity

--- app/src/main/java/com/example/pogoda/MainActivity.java
+++ app/src/main/java/com/example/pogoda/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pogoda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView temperatura = findViewById(R.id.temperatura);
+        TextView wilgotnosc = findViewById(R.id.wilgotnosc);
+        TextView predkosc = findViewById(R.id.predkosc);
+        temperatura.setText("20st" /*do dorobienia z shared albo sqlite*/);
+        temperatura.setText("50%" /*do dorobienia z shared albo sqlite*/);
+        temperatura.setText("5m/s" /*do dorobienia z shared albo sqlite*/);
     }
 }
