0d34214796beaead1fdb21f276ba8a3f4c705324
Patryk Szynal <patrykszynal97@gmail.com>
2019-12-01 17:13:23 +0100

    Added previously chosen cities stored in SQLite db. Reworked refresh button layout.

--- app/src/main/java/com/example/pogoda/CitiesListActivity.java
+++ app/src/main/java/com/example/pogoda/CitiesListActivity.java
@@ -1,6 +1,12 @@
 package com.example.pogoda;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Debug;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,25 +21,73 @@ public class CitiesListActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> list;
     ArrayAdapter adapter;
-    String[] version = {"Aestro","Wilno","Konin","Donut","Radom","Cipki","GingerBread","HoneyComb","IceCream Sandwich",
-            "Jelly Bean","Kitkat","Lolipop","Marshmallow","Nought","Oreo","Krakow","Warszawa","Lodz","Rzeszow","Jaslo","Jodlowa"};
-    //Tę liste trzeba z bazki pobrać
+    FeedReaderDbHelper myDatabase;
+//    String[] version = {"Aestro","Wilno","Konin","Donut","Radom","Cipki","GingerBread","HoneyComb","IceCream Sandwich",
+//            "Jelly Bean","Kitkat","Lolipop","Marshmallow","Nought","Oreo","Krakow","Warszawa","Lodz","Rzeszow","Jaslo","Jodlowa"};
+//    //Tę liste trzeba z bazki pobrać
     SearchView searchView;
+
+    public class FeedReaderDbHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "CITIES";
+
+        public FeedReaderDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE IF NOT EXISTS CITIES_T(Name VARCHAR)");
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL("DROP TABLE CITIES_T");
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        list = new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities_list);
+        myDatabase = new FeedReaderDbHelper(this);
+        SQLiteDatabase w = myDatabase.getWritableDatabase();
+        SQLiteDatabase readable = myDatabase.getReadableDatabase();
+
+        String[] projection = {
+                "Name"
+        };
+
+        String sortOrder =
+                "Name" + " ASC";
+
+        Cursor result = readable.query(
+                "CITIES_T",   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+        while(result.moveToNext()) {
+            String itemId = result.getString(
+                    result.getColumnIndexOrThrow("Name"));
+            list.add(itemId);
+        }
+        result.close();
 
 
         listView = findViewById(R.id.list_view);
         searchView = findViewById(R.id.searchView);
 
-        list = new ArrayList<>();
-
-        for (int i = 0;i<version.length;i++){
-            list.add(version[i]);
-        }
+//        for (int i = 0;i<version.length;i++){
+//            list.add(version[i]);
+//        }
 
         adapter = new ArrayAdapter(CitiesListActivity.this,android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
@@ -46,6 +100,7 @@ public class CitiesListActivity extends AppCompatActivity {
                 bundle.putString("SELECTED_CITY",adapterView.getItemAtPosition(i).toString());
                 myIntent.putExtras(bundle);
                 CitiesListActivity.this.startActivity(myIntent);
+
             }
         });
 
@@ -55,6 +110,9 @@ public class CitiesListActivity extends AppCompatActivity {
                 Bundle bundle = new Bundle();
                 Intent myIntent = new Intent(CitiesListActivity.this, MainActivity.class);
                 bundle.putString("SELECTED_CITY",s);
+                ContentValues values = new ContentValues();
+                values.put("Name", s);
+                myDatabase.getWritableDatabase().insert("CITIES_T", null, values);
                 myIntent.putExtras(bundle);
                 CitiesListActivity.this.startActivity(myIntent);
                 return true;
--- app/src/main/java/com/example/pogoda/MainActivity.java
+++ app/src/main/java/com/example/pogoda/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.IntentFilter;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.*;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.pogoda.receivers.ConnectionReceiver;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver MyReceiver = null;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         SharedPreferences pref = getApplicationContext().getSharedPreferences("WeatherAppPref", 0); // 0 - for private mode
